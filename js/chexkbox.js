e03c6537b79a26e64b3b894b87e3e1fe7d51c2f1
doeeelomda <delomda8@gmail.com>
2023-11-20 16:13:45 +0200

    doaa

--- js/chexkbox.js
+++ js/chexkbox.js
@@ -4,51 +4,160 @@ const next = document.getElementById('next');
 const circles = document.querySelectorAll('.circle');
 
 let currentActive = 1;
+let accumulatedPercentage = 0;
+
 
 next.addEventListener('click', () => {
     currentActive++;
-    if(currentActive > circles.length) {
-        currentActive = circles.length
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
     }
 
-    update()
-})
+    updatenext();
+});
 
 back.addEventListener('click', () => {
     currentActive--;
-    if(currentActive < 1) {
-        currentActive = 1
+    if (currentActive < 1) {
+        currentActive = 1;
     }
 
-    update()
-})
+    updateback();
+});
 
-function update() {
+function updatenext() {
+    const increment = 1 / (circles.length - 2) * 100;
+    accumulatedPercentage += increment;
+
     circles.forEach((circle, index) => {
-        if(index < currentActive) {
-            circle.classList.add('active')
+        if (index < currentActive) {
+            circle.classList.add('active');
         } else {
-            circle.classList.remove('active')
+            circle.classList.remove('active');
         }
-    })
+    });
 
-    const actives = document.querySelectorAll('.active')
-    progress.style.width = ((actives.length - 3) /9)  * 100  + '%';
-    console.log(progress.style.width)
-    progress.style.backgroundColor="#8CA17A"
-    if(currentActive === 1) {
-        back.disabled = true;
-    } else if(currentActive === circles.length) {
-        next.disabled = true;
-    } else {
-        back.disabled = false;
-        next.disabled = false;
+    progress.style.width = accumulatedPercentage + '%';
+    progress.style.backgroundColor = "#8CA17A";
+    console.log( progress.style.width)
+    if (currentActive === 2) {
+        hiddenshapping()
+    } else if (currentActive === 3) {
+        summaryshow()
+    } 
+    else if (currentActive === 4) {
+        successshow()
+        next.disabled=true
     }
 }
+
+function updateback() {
+    const increment = 1 / (circles.length - 1) * 100;
+    accumulatedPercentage -= increment;
+
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    progress.style.width = accumulatedPercentage + '%';
+    progress.style.backgroundColor = "#8CA17A";
+    console.log( progress.style.width)
+
+    if (currentActive === 1) {
+        hiddenpayment()
+        back.disabled=true
+        // back.display='none'
+
+    }
+    else if(currentActive ===0){
+        back.style.visibility='hidden'
+    }
+    else if(currentActive === 2) {
+        hiddensummary()
+    }
+    else if(currentActive === 3) {
+            hiddensucess()
+}
+}
+
 function hiddenshapping(){
     var shapping =document.querySelector('.shapping');
     shapping.style.display="none"
     var paymentchechbox=document.querySelector('.paymentchechbox')
     paymentchechbox.style.display="block"
+
+}
+function summaryshow(){
+    var paymentchechbox=document.querySelector('.paymentchechbox')
+    paymentchechbox.style.display="none";
+    var summary=document.querySelector('.summary');
+    summary.style.display="block"
+
+}
+function successshow(){
+    var summary=document.querySelector('.summary')
+    summary.style.display="none";
+    
+
+    var sucess=document.querySelector('.sucess');
+    sucess.style.display="block"
+
+    let back=document.querySelector('#back')
+    back.style.display='none'
+
+    let next=document.querySelector('#next')
+    next.style.display='none'
+
+}
+
+
+
+
+function hiddenpayment(){
+    var shapping =document.querySelector('.shapping');
+    shapping.style.display="block";
+    var back=document.querySelector('#back')
+    back.style.display='none'
+    var paymentchechbox=document.querySelector('.paymentchechbox');
+    paymentchechbox.style.display="none";
+
+
    
 }
+function hiddensummary(){
+    var paymentchechbox=document.querySelector('.paymentchechbox')
+    paymentchechbox.style.display="block";
+    var summary=document.querySelector('.summary');
+    summary.style.display="none"
+
+}
+
+function  hiddensucess(){
+    var summary=document.querySelector('.summary')
+   summary.style.display="block";
+
+   var sucess=document.querySelector('.sucess');
+   sucess.style.display="none"
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////
