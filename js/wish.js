c3842f3259d0f487b62dc760a6448a68e2499deb
doeeelomda <delomda8@gmail.com>
2023-11-21 16:34:35 +0200

    doaa elomda

--- js/wish.js
+++ js/wish.js
@@ -13,12 +13,12 @@ function addresssetting() {
   function wishlist() {
     var wishlist = document.querySelector('.wishlist');
     wishlist.style.backgroundColor = wishlist.style.backgroundColor === 'rgba(113, 140, 89, 0.12)' ? 'none' : 'rgba(113, 140, 89, 0.12)';
-  var returnhidden=document.querySelector('.returnhidden')
-  returnhidden.style.display = returnhidden.style.display === 'block' ? 'none' : 'block';
-  var myorders=document.querySelector('.my-orders');
-  myorders.style.display=myorders.style.display === 'none' ? 'block' : 'none';
-  var alsolike=document.querySelector('.alsolike');
-  alsolike.style.display=alsolike.style.display === 'none' ? 'block' : 'none'; 
+  // var returnhidden=document.querySelector('.returnhidden')
+  // returnhidden.style.display = returnhidden.style.display === 'block' ? 'none' : 'block';
+  // var myorders=document.querySelector('.my-orders');
+  // myorders.style.display=myorders.style.display === 'none' ? 'block' : 'none';
+  // var alsolike=document.querySelector('.alsolike');
+  // alsolike.style.display=alsolike.style.display === 'none' ? 'block' : 'none'; 
   wishlist.style.padding="20px" 
 }
   function toggleCheckboxes() {
